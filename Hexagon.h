f8a4d78c40db07678672899f6a637559c4f4a5cb
brownzoem <brownzoem@gmail.com>
2021-12-06 18:37:59 -0500

    Create Hexagon.h

---
+++ Hexagon.h
@@ -0,0 +1,27 @@
+//
+// Created by Zoe Brown on 12/6/21.
+//
+
+#ifndef SPHERE_HEXAGON_H
+#define SPHERE_HEXAGON_H
+
+#endif //SPHERE_HEXAGON_H
+class Hexagon {
+private:
+
+public:
+    Hexagon(){};
+    void print();
+
+};
+void Hexagon::print() {
+    cout << "     -----   " << endl;
+    // cout << endl;
+    cout << "   -       -  " << endl;
+    // cout << endl;
+    cout << " -           -  " << endl;
+//    cout << endl;
+    cout << "   -       -  " << endl;
+    //   cout << endl;
+    cout << "     -----   " << endl;
+}
